18cd53e6fbd50ad47e965d797012b668c9929098
Ilya <84504499+visilya@users.noreply.github.com>
2022-11-20 21:50:41 +0300

    Sun 20 Nov 21:50:41 MSK 2022

--- script/CanBoot.sh
+++ script/CanBoot.sh
@@ -3,4 +3,4 @@
 cd ~/CanBoot
 make clean
 make KCONFIG_CONFIG=~/klipper_config/script/CanBoot.cfg
-
+sudo dfu-util -a 0 -D ~/CanBoot/out/canboot.bin --dfuse-address 0x08000000:force:mass-erase:leave -d 0483:df11
