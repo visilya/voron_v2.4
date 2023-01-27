dd924dfb8d805616adcf3c6ee79c73b75bc51d8b
Ilya <84504499+visilya@users.noreply.github.com>
2023-01-27 21:39:16 +0300

    Fri 27 Jan 21:39:16 MSK 2023

--- klipper_update.sh
+++ klipper_update.sh
@@ -1,35 +1,82 @@
 #!/bin/bash
+### Start config ###
+MCU_NAME=mcu
 
-# sudo service klipper stop
+OCTOPUS_NAME=octopus
+OCTOPUS_CAN=ce5f75f5c4f0
+OCTOPUS_SERIAL_ID=usb-CanBoot_stm32f429xx_2E004E001450304738313820-if00
 
-cd ~/klipper/
-git pull
-make clean
+EBB_NAME=ebb1
+EBB_CAN=d22185cfd0c4
 
-sudo sh ~/gpio.sh write 20 1 # dfu mode on
-sudo sh ~/gpio.sh write 21 0 # reset on
-sleep 1
-sudo sh ~/gpio.sh write 21 1 # reset off
-make flash FLASH_DEVICE=0483:df11
-sudo sh ~/gpio.sh write 20 0 # dfu mode off
-sudo sh ~/gpio.sh write 21 0 # reset on
-sleep 1
-sudo sh ~/gpio.sh write 21 1 # reset off
+WORKING_DIR=/home/ilya
+### End config ###
 
-# Update MCU if you need
-# sudo service klipper_mcu stop
-cd ~/klipper_mcu/
-git pull
-make clean
-make flash
-# sudo service klipper_mcu start
 
-# Update EBB if you need
-cd ~/klipper_ebb/
-git pull
-make clean
-make
-python3 ~/CanBoot/scripts/flash_can.py -i can0 -f ./out/klipper.bin -u d22185cfd0c4
+ask=$1
+forceflash=$2
 
-sudo systemctl restart klipper_mcu klipper
+build_klipper() {
+  echo ----------- $1 ----------- 
+  KCONFIG_FILE=$WORKING_DIR/klipper_config/script/klipper_$1.cfg
+  if [ ! -f "$KCONFIG_FILE" ]; then
+    if [[ "$ask" == "1" ]]; then
+      echo "$KCONFIG_FILE does not exists."
+      read -p "Do you want to configure $1? [Y:n]" -n 1 -r REPLY
+      REPLY=${REPLY:-Y}
+      if [[ $REPLY =~ ^[Yy]$ ]]; then
+        make menuconfig KCONFIG_CONFIG=$KCONFIG_FILE
+      else 
+        read -p "Abort bash file? [y:N]" -n 1 -r REPLY
+        REPLY=${REPLY:-N}
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+          exit 1
+        fi
+      fi
+    else
+      echo "$KCONFIG_FILE does not exists."
+      exit 1
+    fi
+  fi
+  if [ -f "$KCONFIG_FILE" ]; then
+    # sed -i -e '1iOUT=out_'"$1"'/' -e '/OUT=/d' $KCONFIG_FILE
+    sed -i -e '/OUT=/d' $KCONFIG_FILE
+    make clean KCONFIG_CONFIG=$KCONFIG_FILE -e OUT=out_"$1"/
+    if [[ "$2" == "build" ]]; then
+      make KCONFIG_CONFIG=$KCONFIG_FILE -e OUT=out_"$1"/
+    fi
+  fi
+}
 
+main() {
+  cd $WORKING_DIR/klipper/
+  klipper_ver=$(git rev-parse HEAD)
+  git pull
+  if [[ $klipper_ver == $(git rev-parse HEAD)]] && ! [["$forceflash" == "1" ]]; then
+    # Same klipper version. 
+    echo "Exiting."
+    exit 0
+  fi
+
+  make clean
+
+  build_klipper $MCU_NAME
+  rm -rf $WORKING_DIR/klipper/out
+  mkdir $WORKING_DIR/klipper/out
+  mv $WORKING_DIR/klipper/out_$MCU_NAME $WORKING_DIR/klipper/out
+  make flash KCONFIG_CONFIG=$KCONFIG_FILE
+  mkdir $WORKING_DIR/klipper/out_$MCU_NAME
+  mv $WORKING_DIR/klipper/out $WORKING_DIR/klipper/out_$MCU_NAME
+
+  build_klipper $OCTOPUS_NAME build
+  python3 $WORKING_DIR/CanBoot/scripts/flash_can.py -i can0 -f $WORKING_DIR/klipper/out_$OCTOPUS_NAME/klipper.bin -u $OCTOPUS_CAN
+  python3 $WORKING_DIR/CanBoot/scripts/flash_can.py -f $WORKING_DIR/klipper/out_$OCTOPUS_NAME/klipper.bin -d /dev/serial/by-id/$OCTOPUS_SERIAL_ID
+
+  build_klipper $EBB_NAME build
+  python3 $WORKING_DIR/CanBoot/scripts/flash_can.py -i can0 -f $WORKING_DIR/klipper/out_$EBB_NAME/klipper.bin -u $EBB_CAN
+  $WORKING_DIR/klippy-env/bin/python $WORKING_DIR/klipper/scripts/canbus_query.py can0
+
+}
+
+main
+exit 0
