c9b232093c62482ae0aaec1f03fc54f5c848b100
ilya <write2ilya@gmail.com>
2023-11-14 19:50:29 +0300

    Tue 14 Nov 19:50:29 MSK 2023

--- script/klipper_update.sh
+++ script/klipper_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Get CAN devicies id
 # ~/klippy-env/bin/python ~/klipper/scripts/canbus_query.py can0
-# python3 ~/CanBoot/scripts/flash_can.py -i can0 -q
+# python3 ~/katapult/scripts/flash_can.py -i can0 -q
 # https://www.shellcheck.net/
 
 ### Start config ###
@@ -9,7 +9,7 @@ MCU_NAME=mcu
 
 OCTOPUS_NAME=octopus
 OCTOPUS_CAN=ce5f75f5c4f0
-# OCTOPUS_SERIAL_ID=usb-CanBoot_stm32f429xx_2E004E001450304738313820-if00
+# OCTOPUS_SERIAL_ID=usb-katapult_stm32f429xx_2E004E001450304738313820-if00
 
 EBB_NAME=ebb
 EBB_CAN=d22185cfd0c4
@@ -53,7 +53,7 @@ build_klipper() {
     sed -i -e '/OUT=/d' "$KCONFIG_FILE"
     make clean KCONFIG_CONFIG="$KCONFIG_FILE" OUT=out_"$1"/
     if [[ "$2" == "build" ]]; then
-      make KCONFIG_CONFIG="$KCONFIG_FILE" OUT=out_"$1"/
+      make -j4 KCONFIG_CONFIG="$KCONFIG_FILE" OUT=out_"$1"/
     fi
     if [[ "$2" == "flash" ]]; then
       echo make flash "$3" KCONFIG_CONFIG="$KCONFIG_FILE" OUT=out_"$1"/
@@ -68,7 +68,7 @@ main() {
   flashebb=1
   forceflashebb=0
 
-  cd $WORKING_DIR/CanBoot/ || exit
+  cd $WORKING_DIR/katapult/ || exit
   canboot_ver=$(git rev-parse HEAD)
   git pull
   if [[ ! ($canboot_ver == $(git rev-parse HEAD))]]; then
@@ -76,12 +76,12 @@ main() {
     if check_kconfig "$KCONFIG_FILE"; then
       if [ -f "$KCONFIG_FILE" ]; then
         make clean KCONFIG_CONFIG="$KCONFIG_FILE" OUT=out_$EBB_NAME/
-        make KCONFIG_CONFIG="$KCONFIG_FILE" OUT=out_$EBB_NAME/
-        python3 $WORKING_DIR/CanBoot/scripts/flash_can.py -i can0 -f $WORKING_DIR/CanBoot/out_$EBB_NAME/deployer.bin -u $EBB_CAN
+        make -j4 KCONFIG_CONFIG="$KCONFIG_FILE" OUT=out_$EBB_NAME/
+        python3 $WORKING_DIR/katapult/scripts/flash_can.py -i can0 -f $WORKING_DIR/katapult/out_$EBB_NAME/deployer.bin -u $EBB_CAN
         forceflashebb=1
       fi
     else
-      echo "$KCONFIG_FILE does not exists. Skipping CanBoot update."
+      echo "$KCONFIG_FILE does not exists. Skipping katapult update."
     fi
   fi
 
@@ -114,7 +114,7 @@ main() {
   if [ $flashebb -eq 1 ]; then
     build_klipper $EBB_NAME build
     ### Flash EBB
-    python3 $WORKING_DIR/CanBoot/scripts/flash_can.py -i can0 -f $WORKING_DIR/klipper/out_$EBB_NAME/klipper.bin -u $EBB_CAN
+    python3 $WORKING_DIR/katapult/scripts/flash_can.py -i can0 -f $WORKING_DIR/klipper/out_$EBB_NAME/klipper.bin -u $EBB_CAN
     ### List CAN devices
     echo "List CAN devices"
     $WORKING_DIR/klippy-env/bin/python $WORKING_DIR/klipper/scripts/canbus_query.py can0
@@ -122,19 +122,19 @@ main() {
   if [ $flashboard -eq 1 ]; then 
     #build_klipper $OCTOPUS_NAME build
     ### Flash Octopus
-    #python3 $WORKING_DIR/CanBoot/scripts/flash_can.py -i can0 -f $WORKING_DIR/klipper/out_$OCTOPUS_NAME/klipper.bin -u $OCTOPUS_CAN
-    #python3 $WORKING_DIR/CanBoot/scripts/flash_can.py -f $WORKING_DIR/klipper/out_$OCTOPUS_NAME/klipper.bin -d /dev/serial/by-id/$OCTOPUS_SERIAL_ID
-    sudo sh $WORKING_DIR/gpio.sh write 20 1 # dfu mode on
-    sudo sh $WORKING_DIR/gpio.sh write 21 0 # reset on
+    #python3 $WORKING_DIR/katapult/scripts/flash_can.py -i can0 -f $WORKING_DIR/klipper/out_$OCTOPUS_NAME/klipper.bin -u $OCTOPUS_CAN
+    #python3 $WORKING_DIR/katapult/scripts/flash_can.py -f $WORKING_DIR/klipper/out_$OCTOPUS_NAME/klipper.bin -d /dev/serial/by-id/$OCTOPUS_SERIAL_ID
+    sudo sh $WORKING_DIR/printer_data/config/script/gpio.sh write 20 1 # dfu mode on
+    sudo sh $WORKING_DIR/printer_data/config/script/gpio.sh write 21 0 # reset on
     sleep 0.5
-    sudo sh $WORKING_DIR/gpio.sh write 21 1 # reset off
+    sudo sh $WORKING_DIR/printer_data/config/script/gpio.sh write 21 1 # reset off
     sleep 1
-    sudo sh $WORKING_DIR/gpio.sh write 20 0 # dfu mode off
-#    python3 ~/CanBoot/scripts/flash_can.py -i can0 -r -u $OCTOPUS_CAN
+    sudo sh $WORKING_DIR/printer_data/config/script/gpio.sh write 20 0 # dfu mode off
+#    python3 ~/katapult/scripts/flash_can.py -i can0 -r -u $OCTOPUS_CAN
     build_klipper $OCTOPUS_NAME flash FLASH_DEVICE=0483:df11
-    sudo sh $WORKING_DIR/gpio.sh write 21 0 # reset on
+    sudo sh $WORKING_DIR/printer_data/config/script/gpio.sh write 21 0 # reset on
     sleep 0.5
-    sudo sh $WORKING_DIR/gpio.sh write 21 1 # reset off
+    sudo sh $WORKING_DIR/printer_data/config/script/gpio.sh write 21 1 # reset off
     sleep 1
   fi
 
